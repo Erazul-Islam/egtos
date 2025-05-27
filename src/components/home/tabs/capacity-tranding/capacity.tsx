c3047ed6ca9a0e76d25cdd402cb4698678771d27
Erazul Islam <erazul89@gmail.com>
2025-05-28 01:25:58 +0600

    add tabs section

--- src/components/home/tabs/capacity-tranding/capacity.tsx
+++ src/components/home/tabs/capacity-tranding/capacity.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GainValue from './gain-value';
+import FirstExperts from './find-experts';
 
 const Capacity = () => {
     return (
         <div>
             <GainValue/>
+            <FirstExperts/>
         </div>
     );
 };
---
+++ src/components/home/tabs/capacity-tranding/find-experts.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FirstExperts = () => {
+    return (
+        <div className="bg-[url('/home/capacity/capacity-bg.png') md:py-12 bg-cover]">
+            <div className='flex justify-between max-w-[1440px] mx-auto px-3 md:px-5'>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FirstExperts;

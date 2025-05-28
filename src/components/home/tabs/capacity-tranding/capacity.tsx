cd282632cb4392dee5f8c85d914e9a55346351bc
Erazul Islam <erazul89@gmail.com>
2025-05-28 14:11:18 +0600

    added

--- src/components/home/tabs/capacity-tranding/capacity.tsx
+++ src/components/home/tabs/capacity-tranding/capacity.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import GainValue from './gain-value';
-import FirstExperts from './find-experts';
-import SaveCosts from './save-costs';
+import React from "react";
+import GainValue from "./gain-value";
+import FirstExperts from "./find-experts";
+import SaveCosts from "./save-costs";
+import BoostCareer from "./boost-career";
+import CustomFooter from "@/components/custom/custom-footer";
+import EgtosButton from "@/components/custom/egtos-button";
+import Image from "next/image";
 
 const Capacity = () => {
-    return (
-        <div>
-            <GainValue/>
-            <FirstExperts/>
-            <SaveCosts/>
-        </div>
-    );
+  return (
+    <div>
+      <GainValue />
+      <FirstExperts />
+      <SaveCosts />
+      <BoostCareer />
+      <CustomFooter
+        button2={
+          <EgtosButton
+            className="px-2"
+            value={<Image src={"/arrow.png"} width={24} height={24} alt="" />}
+          />
+        }
+        button1={<EgtosButton value={"Start Trading Talent"} />}
+        details=""
+        heading="Ready to Future-Proof Your Workforce?"
+      />
+    </div>
+  );
 };
 
 export default Capacity;

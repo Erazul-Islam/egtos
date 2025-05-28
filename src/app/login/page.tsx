062491df277266a1dfa104cd62a72e1c9a73412f
Erazul Islam <erazul89@gmail.com>
2025-05-28 20:29:47 +0600

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Image src={'/login/2.svg'} width={220} height={334} alt='' />
+        </div>
+    );
+};
+
+export default LoginPage;

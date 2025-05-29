37fc94863757ad783dd89023a5d2065438b18f94
Erazul Islam <erazul89@gmail.com>
2025-05-29 19:25:42 +0600

    company login page and reusable compo done

--- src/app/authentication/components/authentication-page.tsx
+++ src/app/authentication/components/authentication-page.tsx
@@ -1,20 +1,33 @@
-import HeadingDetails from '@/components/custom/heading-details';
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
-const AuthenticationPage = () => {
-    return (
-        <div className='min-h-screen flex md:flex-row flex-col'>
-            <div className='bg-[#F2F4F7] max-w-[548px] w-full'>
-                 <Image className=' flex justify-center' src={'/login/login-1.png'} width={411} height={499} alt='' />
-                 <p className='text-[#00A099] font-semibold text-2xl'>Welcome back, we’ve kept your seat warm</p>
-                 <HeadingDetails value="It’s good to see you again. Let’s pick up where you left off" />
-            </div>
-            <div>
-
-            </div>
+const AuthenticationCompo: React.FC<{
+  heading: string;
+  details: React.ReactNode;
+  content: React.ReactNode;
+}> = ({ heading, details, content }) => {
+  return (
+    <div className="min-h-screen flex md:flex-row flex-col">
+      <div className="bg-[#F2F4F7] order-2 md:order-1 lg:max-w-[548px] w-full flex flex-col justify-between items-center p-4">
+        <div className="flex-1 px-4 md:px-0 flex items-center justify-center">
+          <Image
+            src={"/login/login-1.png"}
+            width={411}
+            height={499}
+            alt="Login Illustration"
+            className="object-contain"
+          />
         </div>
-    );
+        <div className=" px-4 md:px-0 pb-6">
+          <p className="text-[#00A099] max-w-[380px] font-semibold text-2xl">
+            {heading}
+          </p>
+          {details}
+        </div>
+      </div>
+      {content}
+    </div>
+  );
 };
 
-export default AuthenticationPage;
+export default AuthenticationCompo;

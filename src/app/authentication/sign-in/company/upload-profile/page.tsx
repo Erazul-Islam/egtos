bf60d12d376e9c91bc43ee13a78a3951cfde0280
Erazul Islam <erazul89@gmail.com>
2025-05-30 12:48:33 +0600

    upload section

---
+++ src/app/authentication/sign-in/company/upload-profile/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import UploadProfile from './components/upload-profile';
+
+const UploadProfilePage = () => {
+    return (
+        <UploadProfile/>
+    );
+};
+
+export default UploadProfilePage;
